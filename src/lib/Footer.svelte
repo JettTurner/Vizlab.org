3c7f7a7695c317db3b598430ea46587c9739ad08
JettTurner <61423477+JettTurner@users.noreply.github.com>
2025-04-06 15:35:25 -0500

    updated footer
    Linked to github repo

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,16 @@
 <footer class="footer footer-center bg-stone-800 text-stone-400 p-4">
-  <aside>
-    <q> Vizlab.org is Powered by <span class="tracking-widest italic"><a href="https://www.youtube.com/watch?v=e2oSGMLmkjI">Beans™</a> Version 4.2.0 Now with better labels! </span></q>
+  <aside class="flex flex-wrap justify-center gap-2 lg:gap-32 text-center">
+    <q>
+      Want to contribute? Check out 
+      <a href="https://github.com/JettTurner/Vizlab.org" class="tracking-widest italic hover:text-white underline">The Repository</a>
+    </q>
+	<q>
+      Vizlab.org is Powered by 
+      <span class="tracking-widest italic">
+        <a href="https://www.youtube.com/watch?v=e2oSGMLmkjI" class="hover:text-white">Beans™</a> 
+        Version 4.2.0 — Now with better labels!
+      </span>
+    </q> 
+    
   </aside>
 </footer>
