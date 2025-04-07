484bee17f21fc7d52c0f65e010411b42e6d86a40
JettTurner <61423477+JettTurner@users.noreply.github.com>
2025-04-06 23:09:46 -0500

    updated version number

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="footer footer-center bg-stone-800 text-stone-400 p-4">
   <aside class="flex flex-wrap justify-center gap-2 lg:gap-32 text-center">
-    <span class="text-blue-400 tracking-widest italic">Version 4.3.1</span>
+    <span class="text-blue-400 tracking-widest italic">Version 4.3.2</span>
 	<span> Want to contribute? Check out <a href="https://github.com/JettTurner/Vizlab.org" class="tracking-widest italic hover:text-white underline">The Repository</a> </span>
-	<q class="text-red-400"> Now on GitHub!</q>
+	<q class="text-red-400"> Now on GitHub! + Now with better Sidebar Behaviour!</q>
   </aside>
 </footer>
